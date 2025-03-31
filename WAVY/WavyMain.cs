b194d9a0218647720c9f6c19fcab9b9ccabe4de9
Rui Couto <al78555@alunos.utad.pt>
2025-03-31 14:42:52 +0100

    README atualizado

--- WAVY/WavyMain.cs
+++ WAVY/WavyMain.cs
@@ -8,8 +8,15 @@ class WavyMain
         Wavy Wavy02 = new Wavy(AgregadorIP, Port, "WAVY02");
         Wavy Wavy03 = new Wavy(AgregadorIP, Port, "WAVY03");
 
+        // Isto é em sequência, não em paralelo
         Wavy01.Send(Wavy01.WavyID + ", sensor1=25.4, sensor2=30.2, sensor3=18.9");
         Wavy02.Send(Wavy02.WavyID + ", sensor1=25.4, sensor2=30.2, sensor3=18.9");
         Wavy03.Send(Wavy03.WavyID + ", sensor1=25.4, sensor2=30.2, sensor3=18.9");
+
+        
+        // Para simular melhor, partilhar processos/threads
+
+        // Possivelmente tornar os WAVYs modulares
+
     }
 }
