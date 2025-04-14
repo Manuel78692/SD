4c6b54493f0e0eb23aa3cf959b8eb479b46b790b
Rui Couto <al78555@alunos.utad.pt>
2025-04-14 16:56:10 +0100

    Atualizações

--- SERVIDOR/obj/Debug/net8.0/SERVIDOR.AssemblyInfo.cs
+++ SERVIDOR/obj/Debug/net8.0/SERVIDOR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SERVIDOR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600feab0418baeafa9cb0ad351bf395227038221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a6cf05f23da2f755a5f5913a4a25915a2d67d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SERVIDOR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SERVIDOR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
