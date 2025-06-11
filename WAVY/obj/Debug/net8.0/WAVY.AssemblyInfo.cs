bc8b3a62a407137b705116b9a558d7146fce1d5a
Rui Couto <al78555@alunos.utad.pt>
2025-06-12 00:21:13 +0100

    gitignore

--- WAVY/obj/Debug/net8.0/WAVY.AssemblyInfo.cs
+++ WAVY/obj/Debug/net8.0/WAVY.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f60a029364b0695adc785e5975320608b29e1567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a957e03c20c03624279dfe579468e7d4be9028")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAVY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
